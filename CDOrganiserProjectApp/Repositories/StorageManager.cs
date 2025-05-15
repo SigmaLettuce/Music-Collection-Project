53cab053b27bf39a0831bd5dd689a901c41889fb
Magnus Lopez <ac148904@avcol.school.nz>
2025-05-16 09:34:07 +1200

    Began adding inline comments.

--- CDOrganiserProjectApp/Repositories/StorageManager.cs
+++ CDOrganiserProjectApp/Repositories/StorageManager.cs
@@ -2,6 +2,7 @@
 
 namespace CDOrganiserProjectApp.Repositories
 {
+    // Manages connection to database, passes queries etc.
     public class StorageManager
     {
         private SqlConnection conn;
