fc64e5afe2f540bd1dd8eb5f3af45a73b34f4c5d
Magnus Lopez <ac148904@avcol.school.nz>
2025-05-16 09:46:18 +1200

    Created exception handling constructor.

--- CDOrganiserProjectApp/Repositories/StorageManager.cs
+++ CDOrganiserProjectApp/Repositories/StorageManager.cs
@@ -7,6 +7,20 @@ namespace CDOrganiserProjectApp.Repositories
     {
         private SqlConnection conn;
 
+        public StorageManager(string connectionStr)
+        {
+            try
+            {
+                conn = new SqlConnection(connectionStr);
+                conn.Open();
+                Console.WriteLine("Connection successful.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection failed.\n");
+                Console.WriteLine(ex.Message);
+            }
 
+        }
     }
 }
