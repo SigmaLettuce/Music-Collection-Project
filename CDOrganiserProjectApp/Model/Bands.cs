5a5dd1a3a6f756fca8692a0153dcfad4ba91f3f4
Magnus Lopez <ac148904@avcol.school.nz>
2025-05-15 20:38:57 +1200

    Created first table class.

--- CDOrganiserProjectApp/Model/Bands.cs
+++ CDOrganiserProjectApp/Model/Bands.cs
@@ -7,7 +7,7 @@ namespace CDOrganiserProjectApp.Model
 
         public string bandName {  get; set; }   
         public string bandID { get; set; }  
-
+         
         public Bands(string bn, string bid)
         {
             bandName = bn;
